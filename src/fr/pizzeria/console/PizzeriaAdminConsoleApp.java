171648c730924f305bde8351c7b440d0996a6261
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-07-05 10:26:58 +0200

    Second commit

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -89,7 +89,6 @@ public class PizzeriaAdminConsoleApp {
 				}
 			}
 		}
-		
 	}
 
 	/**
@@ -153,7 +152,6 @@ public class PizzeriaAdminConsoleApp {
 										codeString,
 										nomString,
 										prixString};
-		
 	}
 
 	/**
